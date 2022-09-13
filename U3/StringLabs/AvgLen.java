ef7e927f6a1f9bb06351d469e9cdfe86268c87b0
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-09-13 16:18:09 -0500

    Added String labs

---
+++ U3/StringLabs/AvgLen.java
@@ -0,0 +1,11 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+		return (a.length()+b.length())/2;
+	}
+}
